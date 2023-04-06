bef3f295e898f556c2a39f547b96764f95b85918
Isaac <123217532+iamiller-cyber@users.noreply.github.com>
2023-04-06 08:26:49 -0400

    Add eventListener that changes div class

--- java.js
+++ java.js
@@ -1,17 +1,12 @@
 const container = document.querySelector('.container')
 
 for (let i = 1; i <= 256; i++) {
-    const grid = document.createElement('div')
+    let grid = document.createElement('div')
     grid.className = 'grid'
     container.appendChild(grid)    
 }
 
-/* grid.addEventListener('mouseover', function(e) {
-    if (e.target.idList !== 'sketch') {
-        e.target.addId('sketch')
-    }
-})
-
-function changeColor() {
-
-}
+let grid = document.querySelectorAll('.grid')
+Array.from(grid).forEach(item => item.addEventListener('mouseenter', function(e) {
+    e.target.setAttribute('class', 'sketch')
+}))
