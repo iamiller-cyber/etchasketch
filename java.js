f43cb53ef3511032d4723ff938519a384b8d07b0
Isaac <123217532+iamiller-cyber@users.noreply.github.com>
2023-04-07 09:10:54 -0400

    newGrid removes old grid

--- java.js
+++ java.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container')
+const button = document.querySelector('button')
 
 for (let i = 1; i <= 256; i++) {
     let grid = document.createElement('div')
@@ -10,3 +11,25 @@ let grid = document.querySelectorAll('.grid')
 Array.from(grid).forEach(item => item.addEventListener('mouseenter', function(e) {
     e.target.setAttribute('class', 'sketch')
 }))
+
+button.addEventListener('click', newGrid)
+
+function newGrid() {
+    let grid = document.querySelectorAll('.grid')
+    Array.from(grid).forEach(grid => container.removeChild(grid))
+/*   let size = prompt('How big you want this grid? Max 100 pls')
+    if (isNaN(size)) {
+        alert('Has to be a number!')
+        return
+    }
+    if (size > 100) {
+        alert('I said <100 pls')
+        return
+    }
+    else for (let i = 1; i < (size * size); i++) {
+        let grid = document.createElement('div')
+        grid.className = 'newGrid'
+        container.appendChild(grid)
+    }
+} */ 
+// not ready yet
